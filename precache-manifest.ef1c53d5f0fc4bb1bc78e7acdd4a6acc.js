e791791d2c73bf9b5e40c7180a33ed22da5c67fb
Anirudh Nagasamudra <aninaga56@gmail.com>
2023-10-07 21:27:33 -0700

    Updates

---
+++ precache-manifest.ef1c53d5f0fc4bb1bc78e7acdd4a6acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016b5926866409dfd30eb1239fcb19ef",
+    "url": "/todo-webapp/index.html"
+  },
+  {
+    "revision": "fe013ef9321d9b4fb744",
+    "url": "/todo-webapp/static/css/main.b79a8851.chunk.css"
+  },
+  {
+    "revision": "e0cd165c90a0b30b8f79",
+    "url": "/todo-webapp/static/js/2.18638a81.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/todo-webapp/static/js/2.18638a81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe013ef9321d9b4fb744",
+    "url": "/todo-webapp/static/js/main.ce6caa07.chunk.js"
+  },
+  {
+    "revision": "c04cfa62596120d7cf96",
+    "url": "/todo-webapp/static/js/runtime-main.811cf03d.js"
+  }
+]);
